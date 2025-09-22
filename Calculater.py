2ef32eff55fe4a1ca2388f257c7330cbcf2875bf
Barnali Tanti <tantibarnali520@gmail.com>
2025-09-22 21:12:20 +0530

    Add files via upload

---
+++ Calculater.py
@@ -0,0 +1,15 @@
+#Python claculator
+
+operator=input("Enter an operator(+ - * /):")
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+
+if operator=="+":
+    print(num1+num2)
+elif operator=="-":
+    print(num1-num2)
+elif operator=="*":
+    print(num1*num2)
+else:
+    print(num1/num2)
+
