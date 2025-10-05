b6a8dd3ef4e1b0f48999b40ebcb364e5807b4d0a
Barnali Tanti <tantibarnali520@gmail.com>
2025-10-05 19:23:16 +0530

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,8 @@
+
+row= int(input("Entre the no. of rows: "))
+col = int(input("Entre the no. of columns: "))
+symbol= input("Entre the symbol: ")
+for i in range(row):
+    for j in range(col):
+        print(symbol,end="")
+    print()
