e49bea81290dd834b02916a135c6e573cc45cf9d
Barnali Tanti <tantibarnali520@gmail.com>
2026-05-07 22:30:15 +0530

    Add files via upload

---
+++ Matrix_array.py
@@ -0,0 +1,19 @@
+X = [[1, 2, 3],
+     [2, 3, 4],
+     [3, 4, 5]]
+
+Y = [[1, 2, 3],
+     [2, 3, 4],
+     [3, 4, 5]]
+# result is 3x3
+result = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+for i in range(len(X)):
+
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+    print(r)
